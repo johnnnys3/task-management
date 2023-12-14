0d75410bd592f8bd1ea60987f3bb496d98e6cf63
johnnnys3 <johnkessie04@gmail.com>
2023-12-14 12:18:19 +0000

    calendar_integration

--- lib/screens/calendar_integration_screen.dart
+++ lib/screens/calendar_integration_screen.dart
@@ -2,41 +2,33 @@
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 
-
-
 class CalendarIntegrationScreen extends StatefulWidget {
- @override
- _CalendarIntegrationScreenState createState() => _CalendarIntegrationScreenState();
+  @override
+  _CalendarIntegrationScreenState createState() => _CalendarIntegrationScreenState();
 }
 
 class _CalendarIntegrationScreenState extends State<CalendarIntegrationScreen> {
- late CalendarController _calendarController;
+  DateTime _selectedDay = DateTime.now();
 
- @override
- void initState() {
-    super.initState();
-    _calendarController = CalendarController();
- }
-
- @override
- void dispose() {
-    _calendarController.dispose();
-    super.dispose();
- }
-
- @override
- Widget build(BuildContext context) {
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Calendar Integration'),
       ),
       body: TableCalendar(
-        calendarController: _calendarController,
-        // Add more configuration as needed
-        focusedDay: DateTime.now(),
+        selectedDayPredicate: (day) {
+          return isSameDay(_selectedDay, day);
+        },
+        onDaySelected: (selectedDay, focusedDay) {
+          setState(() {
+            _selectedDay = selectedDay;
+          });
+        },
+        focusedDay: _selectedDay, // Provide the required focusedDay parameter
         firstDay: DateTime(2023, 1, 1),
         lastDay: DateTime(2023, 12, 31),
       ),
     );
- }
+  }
 }
